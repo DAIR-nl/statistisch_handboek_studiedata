6f94b3e8fd49645d2464871c25a94b1496534389
Theo Bakker <t.c.bakker@vu.nl>
2019-07-26 16:48:31 +0200

    De eerste bestanden van dit fantastische project

---
+++ Index.R
@@ -0,0 +1,49 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Index.R
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code voor Student Analytics Vrije Universiteit Amsterdam
+## Copyright 2019 VU
+## Web Page: http://www.vu.nl
+## Contact: Theo Bakker (t.c.bakker@vu.nl)
+## Project: Versnellingsagenda - Statistisch Handboek HO
+##
+## Doel: Index bestand voor het uitvoeren van statistische toetsen
+##
+## Afhankelijkheden: Geen
+##
+## Datasets: Geen
+##
+## Opmerkingen: 
+## 1) Geen.
+## 2) ___
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Geschiedenis:
+## 26-07-2019: TB: Aanmaak bestand
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## De hoofdstructuur is:
+## Vooraf: vaststellen van de working directory, inlezen van functies en het aanmaken
+## van directories
+## 1) Voer voorbereidingen uti
+## 2) Voer toetsen uit
+## 3) Sluit af en ruim op
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## 01 VOORBEREIDINGEN ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## Laad hier de nodige bibliotheken in
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## 02 TOETSEN ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+source("01. Toetsen/One sample T-toets.R", echo = TRUE)
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## RUIM OP ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++

d96d4415b82c183f211e740b3752ea8a6748a399
Theo Bakker <t.c.bakker@vu.nl>
2019-09-03 13:53:15 +0200

    Render notebook.R

---
+++ Render notebook.R
@@ -0,0 +1,54 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Render notebook.R
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code voor Student Analytics Vrije Universiteit Amsterdam
+## Copyright 2019 VU
+## Web Page: http://www.vu.nl
+## Contact: Theo Bakker (t.c.bakker@vu.nl)
+## Verspreiding buiten de VU: Ja
+##
+## Doel: Dit script voert het renderen van het notebook uit. De output is een
+## html pagina met alle plots. Na het renderen wordt deze output weggeschreven
+## naar de network directory in de folder Modellen.
+##
+## Afhankelijkheden: Index.R
+##
+## Datasets: Geen
+##
+## Opmerkingen:
+## 1) Geen
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## TODO:
+## 1) Geen
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Geschiedenis:
+## 24-08-2017: JvZ: Aanmaak bestand
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## RENDER NOTEBOOK ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+file.remove("04. Modelleren/Aantal EC tm jaar 2/Exploreren_Aantal_EC.nb.html")
+rmarkdown::render("04. Modelleren/Aantal EC tm jaar 2/Exploreren_Aantal_EC.Rmd")
+
+## Kopieer het bestand naar de network directory
+file.copy("04. Modelleren/Aantal EC tm jaar 2/Exploreren_Aantal_EC.nb.html",
+          paste0(Network_directory,"/Modellen/Exploreren_Aantal_EC",Sys.Date(),".nb.html"),
+          overwrite = T)
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## RENDER BOOKDOWN ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+wd_Origineel <- getwd()
+setwd("04. Modelleren/Aantal EC tm jaar 2/")
+bookdown::render_book("index.Rmd")
+setwd(wd_Origineel)
+
+
+
+
+

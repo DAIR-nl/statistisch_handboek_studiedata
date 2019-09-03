e319bbf795bc5a9ba487d6b46c088e88417b0326
Kerkhoven <mkn502@VU.LOCAL>
2019-09-03 17:53:22 +0200

    index.r aangemaakt

--- Index.R
+++ Index.R
@@ -33,17 +33,72 @@
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## 01 VOORBEREIDINGEN ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+library(stringr)
+library(rlist)
+library(rmarkdown)
 
-## Laad hier de nodige bibliotheken in
+toetsenlijst <- list.files(path = "02. Markdown/")
+toetsenlijst <- sapply("02. Markdown", paste, toetsenlijst,  sep ="/")
+html_indices <- grep(".html", toetsenlijst)
+html_paginas <- toetsenlijst[html_indices]
+toetsenlijst <- toetsenlijst[-html_indices]
+
+
+Bekijk_voortgang <- function(){
+  toetsenlijst <- list.files(path = "02. Markdown/")
+  toetsnummers <- str_extract(toetsenlijst, "[0-9]+")
+  toetsenlijst <- sapply("02. Markdown", paste, toetsenlijst,  sep ="/")
+  html_indices <- grep(".html", toetsenlijst)
+  html_paginas <- toetsenlijst[html_indices]
+  toetsenlijst <- toetsenlijst[-html_indices]
+  
+  versies <- list()
+  for (toetsnaam in toetsenlijst) {
+    toetspagina <- read_file(toetspagina)
+    if (str_detect(tolower("versie 1.0", toetspagina()))) {
+      versie <- 7
+    } else if (str_detect(tolower("versie 0.6", toetspagina))) {
+     versie <- 6
+    } else if (str_detect(tolower("versie 0.5", toetspagina))) {
+      versie <- 5
+    } else if (str_detect(tolower("versie 0.4", toetspagina))) {
+      versie <- 4
+    } else if (str_detect(tolower("versie 0.3", toetspagina))) {
+      versie <- 3
+    } else if (str_detect(tolower("versie 0.2", toetspagina))) {
+      versie <- 2
+    } else if (str_detect(tolower("versie 0.1", toetspagina))) {
+      versie <- 1
+ }
+ append(versies, versie)
+  }
+  
+ voortgang <- data.frame(toetsnummers, versies)
+ ggplot(voortgang, aes(x = toetsen, y = versies)) +
+  geom_barplot(aes(y = versies),
+                 binwidth = 1) +
+  ylab("Versie") +
+  xlab("Toetspagina") +
+  scale_x_continuous(labels = as.character(seq(1, 19)), breaks = seq(1, 19)) +
+  scale_y_discrete(labels = c("Data klaar", "Tekst klaar", "Review Megiel", "Review Theo", "Review VU-A", "Review wetenschap", "Pagina af"))
+  labs(title = "Voortgang SHHO") +
+  coord_flip()
+
+}
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## 02 TOETSEN ####
+## 02 RENDER TOETSEN ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-source("01. Toetsen/One sample T-toets.R", echo = TRUE)
+
+
+sapply(html_paginas, file.remove)
+sapply(toetsenlijst, render)
+
+## als je 1 specifieke pagina wil renderen gebruik je in de console `render(toetsenlijst[toetsnummer]`.
+
+
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## RUIM OP ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-rm(list = ls())

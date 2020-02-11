c633d72c0a9dd7cf3cc6c253121eb32779fdb246
ElianGriffioen <e2.griffioen@vu.nl>
2020-02-11 16:32:29 +0100

    16 gemaakt en 2 bijgewerkt

--- 01. Includes/data/02.R
+++ 01. Includes/data/02.R
@@ -1,26 +1,36 @@
-RNGkind(sample.kind = "Rounding")
+#RNGkind(sample.kind = "Rounding")
 set.seed(2)
 
+# OUD
 ## stel parameters data vast
-MUt0 <- 29
-MUt1 <- 29.2
-SD <- 9
+#MUt0 <- 29
+#MUt1 <- 29.2
+#SD <- 9
+
+# parameters verschil groepen
+MU_verschil <- 5
+SD_verschil <- 2.5
+
+# genereer verschillen met normale verdeling
+Uren_studeren_verschillen <- rnorm(134,MU_verschil,SD_verschil)
 
 ## maak uren studeren aan op basis van een normale verdeling
-Uren_studeren_t0 <- rnorm(134, MUt0, SD)
-Uren_studeren_t1 <- rnorm(134, MUt1, SD)
+Uren_studeren_t0 <- runif(134, 8, 20)
+Uren_studeren_t1 <- Uren_studeren_t0 + Uren_studeren_verschillen
 
 ## Rond uren studeren af naar 1 decimaal
 Uren_studeren_t0 <- sapply(Uren_studeren_t0, round, 1)
 Uren_studeren_t1 <- sapply(Uren_studeren_t1, round, 1)
-
-## stel volgorde vast en rangschik op grootte, voeg daarna samen
-volgorde <- order(Uren_studeren_t0)
-Uren_studeren_t0 <- sort(Uren_studeren_t0)
-Uren_studeren_t1 <- sort(Uren_studeren_t1)
-Uren_studeren_t0 <- Uren_studeren_t0[volgorde]
-Uren_studeren_t1 <- Uren_studeren_t1[volgorde]
 Uren_studeren <- c(Uren_studeren_t0, Uren_studeren_t1)
+
+# OUD
+## stel volgorde vast en rangschik op grootte, voeg daarna samen
+#volgorde <- order(Uren_studeren_t0)
+#Uren_studeren_t0 <- sort(Uren_studeren_t0)
+#Uren_studeren_t1 <- sort(Uren_studeren_t1)
+#Uren_studeren_t0 <- Uren_studeren_t0[volgorde]
+#Uren_studeren_t1 <- Uren_studeren_t1[volgorde]
+#Uren_studeren <- c(Uren_studeren_t0, Uren_studeren_t1)
 
 ## Maak studentnummer aan en verdubbel
 Studentnr <-sample(300000:400000, (length(Uren_studeren_t0)))
@@ -35,5 +45,6 @@ Meetmoment <- as.factor(c(Meetmoment_t0, Meetmoment_t1))
 Studielogboek <- data.frame(Studentnr, Uren_studeren, Meetmoment)
 
 ## MAAK N30 SAMPLE, let op klopt niet helemaal
-Uren_studeren_T0_n30 <- sample(Uren_studeren_t0, 30)
-Uren_studeren_T1_n30 <- sample(Uren_studeren_t1, 30)
+indices <- sample.int(nrow(Studielogboek)/2,30)
+Uren_studeren_verschil_n30 <- Studielogboek$Uren_studeren[indices + nrow(Studielogboek)/2] - Studielogboek$Uren_studeren[indices]
+

157594167c5170a6cb6bb6724679b4abfccb88a5
ElianGriffioen <e2.griffioen@vu.nl>
2020-02-25 17:07:05 +0100

    Feedback verwerken

--- 01. Includes/data/08.R
+++ 01. Includes/data/08.R
@@ -1,5 +1,6 @@
 ## Effectmaat niet gerapporteerd, ik zie geen overeenstemming van effectmaten bij de MW U 
 
+# effectmaat r, correlatie, zie Field blz. 57
 RNGkind(sample.kind = "Rounding")
 set.seed(0)
 
@@ -8,19 +9,19 @@ Matige_student_ec <- c(0, 0, 0, 3, 6, 6, 6, 6, 6)
 Slechte_student_ec <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6)
 
 Resultaten_nl_studenten <- rbind (
-  t(replicate(401, sample(Goede_student_ec, 11, replace = TRUE))), 
-  t(replicate(203, sample(Matige_student_ec, 11, replace = TRUE))),
-  t(replicate(309, sample(Slechte_student_ec, 11, replace = TRUE))))
+  t(replicate(450, sample(Goede_student_ec, 11, replace = TRUE))), 
+  t(replicate(200, sample(Matige_student_ec, 11, replace = TRUE))),
+  t(replicate(300, sample(Slechte_student_ec, 11, replace = TRUE))))
 Punten_jaar2_NL <- margin.table(Resultaten_nl_studenten, 1)
 
 Resultaten_internationale_studenten <- rbind (
-  t(replicate(250, sample(Goede_student_ec, 11, replace = TRUE))), 
+  t(replicate(150, sample(Goede_student_ec, 11, replace = TRUE))), 
   t(replicate(100, sample(Matige_student_ec, 11, replace = TRUE))),
-  t(replicate(200, sample(Slechte_student_ec, 11, replace = TRUE))))
+  t(replicate(300, sample(Slechte_student_ec, 11, replace = TRUE))))
 Punten_jaar2_inter <- margin.table(Resultaten_internationale_studenten, 1)
 Studentnr <- sample(3000000:4000000, length(c(Punten_jaar2_NL, Punten_jaar2_inter)))
 
-Punten_jaar2 <- data.frame(Studentnr, c(Punten_jaar2_NL, Punten_jaar2_inter), 
-                           c(rep("ja", length(Punten_jaar2_NL)), rep("nee", length(Punten_jaar2_inter))))
-colnames(Punten_jaar2)<-c("Studentnr", "EC_Jaar2", "Nederlands")
-Punten_jaar2 <- Punten_jaar2[order(Studentnr),]
+Studiepunten_jaar2 <- data.frame(Studentnr, c(Punten_jaar2_NL, Punten_jaar2_inter), 
+                           c(rep("Nederland", length(Punten_jaar2_NL)), rep("buitenland", length(Punten_jaar2_inter))))
+colnames(Studiepunten_jaar2)<-c("Studentnummer", "Studiepunten", "Vooropleiding")
+Studiepunten_studiejaar2 <- Studiepunten_studiejaar2[order(Studentnr),]

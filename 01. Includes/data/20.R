5d3d6492188ea02237d42fdb3034145cfde6c4e3
ElianGriffioen <e2.griffioen@vu.nl>
2020-04-28 08:59:46 +0200

    Toets gevonden GLMM

--- 01. Includes/data/20.R
+++ 01. Includes/data/20.R
@@ -21,10 +21,10 @@ Periode <- c(rep("P1", 60), rep("P2", 60), rep("P3", 60),
 
 # Maak verdelingen aantal bibliotheken per periode
 Bibliotheken_P1 <- c(rep("Bieb1", 20), rep("Bieb2", 20), rep("Bieb3", 20))
-Bibliotheken_P1 <- c(rep("Bieb1", 30), rep("Bieb2", 20), rep("Bieb3", 10))
-Bibliotheken_P1 <- c(rep("Bieb1", 40), rep("Bieb2", 10), rep("Bieb3", 10))
-Bibliotheken_P1 <- c(rep("Bieb1", 40), rep("Bieb2", 15), rep("Bieb3", 5))
-Bibliotheken_P1 <- c(rep("Bieb1", 45), rep("Bieb2", 10), rep("Bieb3", 5))
+Bibliotheken_P2 <- c(rep("Bieb1", 30), rep("Bieb2", 20), rep("Bieb3", 10))
+Bibliotheken_P3 <- c(rep("Bieb1", 40), rep("Bieb2", 10), rep("Bieb3", 10))
+Bibliotheken_P4 <- c(rep("Bieb1", 40), rep("Bieb2", 15), rep("Bieb3", 5))
+Bibliotheken_P5 <- c(rep("Bieb1", 45), rep("Bieb2", 10), rep("Bieb3", 5))
 
 # Hussel de bibliotheken door elkaar door indexen te simuleren van 0 tot 60
 Indexen_P1 <- sample.int(60, 60) 
@@ -34,9 +34,9 @@ Indexen_P4 <- sample.int(60, 60)
 Indexen_P5 <- sample.int(60, 60) 
 
 # Voeg bibliotheken en indexen samen in een vector
-Bibliotheek <- c(Bibliotheken_P1[Indexen_P1], Bibliotheken_P1[Indexen_P2], 
-                 Bibliotheken_P1[Indexen_P3], Bibliotheken_P1[Indexen_P4], 
-                 Bibliotheken_P1[Indexen_P5])
+Bibliotheek <- c(Bibliotheken_P1[Indexen_P1], Bibliotheken_P2[Indexen_P2], 
+                 Bibliotheken_P3[Indexen_P3], Bibliotheken_P4[Indexen_P4], 
+                 Bibliotheken_P5[Indexen_P5])
 
 # Maak een dataset
 Data_bibliotheken <- data.frame(Studentnummer, Periode, Bibliotheek)
@@ -57,3 +57,33 @@ fit <- MCMCglmm(fixed = Bibliotheek ~ 1, random = ~Studentnummer, data = Data_bi
 
 
 pkgbuild::has_build_tools(debug = TRUE)
+
+## CMH test
+
+mantelhaen.test(Data_bibliotheken$Bibliotheek, Data_bibliotheken$Periode,
+                Data_bibliotheken$Studentnummer, exact = TRUE, correct = TRUE)
+
+?mantelhaen.test
+
+### post hoc
+
+library(rcompanion)
+
+pairwiseMcnemar(Bibliotheek ~ Periode | Studentnummer, data = Data_bibliotheken,
+                test = "mcnemar", correct = FALSE)
+
+# niks significant
+
+# test los paar
+
+subset <- Data_bibliotheken[Data_bibliotheken$Periode %in% c("P1", "P5"),]
+mcnemar.test(Data_bibliotheken$Bibliotheek[Data_bibliotheken$Periode == "P1"],
+             Data_bibliotheken$Bibliotheek[Data_bibliotheken$Periode == "P5"],
+             correct = TRUE)
+
+table(Data_bibliotheken$Bibliotheek[Data_bibliotheken$Periode == "P1"],
+             Data_bibliotheken$Bibliotheek[Data_bibliotheken$Periode == "P5"])
+table(subset$Bibliotheek, subset$Periode)
+
+
+table(Data_bibliotheken$Bibliotheek, Data_bibliotheken$Periode)

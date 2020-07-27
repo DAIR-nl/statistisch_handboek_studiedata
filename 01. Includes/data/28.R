bbb5a4f04e2f34433ca256fa603d8a1ef3cf4ba1
ElianGriffioen <e2.griffioen@vu.nl>
2020-07-27 17:48:14 +0200

    Regressie toetspagina

---
+++ 01. Includes/data/28.R
@@ -0,0 +1,114 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## 28.R  ####
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## R code voor Student Analytics Vrije Universiteit Amsterdam
+## Copyright 2020 VU
+## Web Page: http://www.vu.nl
+## Contact: Theo Bakker (t.c.bakker@vu.nl)
+## Verspreiding buiten de VU: Ja
+##
+## Doel: Doel
+##
+## Afhankelijkheden: Afhankelijkheid
+##
+## Datasets: Datasets
+##
+## Opmerkingen:
+## 1) Geen.
+## 2) ___
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## TODO:
+## 1) ___.
+##
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Geschiedenis:
+## 17-07-2020: EG: Aanmaak bestand
+## ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+## Toets: multipele lineaire regressie
+
+## Casus
+# De docent van het van Methoden & Statistiek van de bachelor Onderwijskunde wil 
+# zijn studenten ervan overtuigen dat het nuttig is om naar zijn colleges te 
+# komen. Hij wil daarom onderzoeken of er een relatie is tussen het aantal 
+# colleges dat studenten volgen en het eindcijfer voor zijn vak. Daarnaast 
+# vermoedt hij dat het eindexamencijfer voor wiskunde ook gerelateerd is aan het 
+# eindcijfer voor het vak en dat er man-vrouw verschillen zouden kunnen zijn. Op 
+# basis van de data van vorig jaar onderzoekt hij of er een relatie is tussen het 
+# aantal hoorcolleges dat studenten volgen, waarbij hij rekening houdt met het 
+# eindexamencijfer wiskunde en man-vrouw verschillen. Met de resultaten hiervan 
+# hoopt hij zijn studenten ervan te overtuigen om de hoorcolleges van het vak 
+# bij te wonen.
+
+# seed
+set.seed(12345)
+
+## Simuleer data
+
+# Aantal hoorcolleges: tussen 1 en 10, zwartepunt bij twee keer en acht keer
+Aantal_hoorcolleges <- sample.int(10, 
+                                  100, 
+                                  replace = TRUE,
+                                  prob = c(0.05,
+                                           0.1,
+                                           0.2,
+                                           0.1,
+                                           0.05,
+                                           0.05,
+                                           0.1,
+                                           0.2,
+                                           0.05,
+                                           0.1))
+
+# Geslacht
+Geslacht <- c(rep("Man", 
+                  20),
+              rep("Vrouw", 
+                  80))[sample.int(100,100)]
+Geslacht_dummy <- rep(1,
+                      100)
+Geslacht_dummy[Geslacht == "Man"] <- 0
+
+# Eindexamencijfer wiskunde middelbare school
+Eindexamencijfer_Wiskunde <- ceiling(2 * rnorm(100,
+                                         7,
+                                         1))/2
+Eindexamencijfer_Wiskunde[Eindexamencijfer_Wiskunde < 5.5] <- 5.5
+
+# Maak afhankelijke variabele eindcijfer Methoden & Statistiek
+Eindcijfer_MS <- 2 + 0.4 * Aantal_hoorcolleges + 0 * Geslacht_dummy + 0.25 * Eindexamencijfer_Wiskunde + rnorm(100,0,1)
+Eindcijfer_MS <- round(Eindcijfer_MS,
+                       1)
+Eindcijfer_MS[Eindcijfer_MS > 10] <- 10.0
+hist(Eindcijfer_MS)
+
+
+sum(Eindcijfer_MS > 5.5)
+
+# Maak studentnummer
+Studentnummer <- sample(300000:400000,
+                            100)
+
+# Maak dataframe
+Studenten_Methoden_Statistiek <- data.frame(Studentnummer,
+                                            Geslacht,
+                                            Eindexamencijfer_Wiskunde,
+                                            Aantal_hoorcolleges,
+                                            Eindcijfer_MS)
+
+# lm
+#model <- lm(Eindcijfer_MS ~ Geslacht + Eindexamencijfer_Wiskunde + Aantal_hoorcolleges,
+#   Studenten_Methoden_Statistiek)
+#summary(model)
+
+rm(Studentnummer,
+   Geslacht,
+   Geslacht_dummy,
+   Eindexamencijfer_Wiskunde,
+   Aantal_hoorcolleges,
+   Eindcijfer_MS
+   )
+
+#vv <- lm(Eindcijfer_MS ~ 1, Studenten_Methoden_Statistiek )
+#summary(vv)
--- 03. Test/Regressie oefenen.R
+++ 03. Test/Regressie oefenen.R
@@ -38,11 +38,12 @@ x1 <- rnorm(100,0,2)
 x2 <- rnorm(100,3,2)
 
 y <- 2.3 + 0.7 * x1 - 0.4 * x2 + rnorm(100,0,1)
+y <- 2.3 + 0 * x1 - 0 * x2 + rnorm(100,0,1)
 
 dat <- data.frame(y,x1,x2)
 
 model <- lm(y ~ x1 + x2)
-
+summary(model)
 # Outliers
 plot(cooks.distance(model))
 
@@ -78,14 +79,15 @@ rms::vif(model)
 
 ### Assumptie normaliteit of heteroskedasticity violated -> bootstrapping
 
+## Parametric, niet helemaal wat we willen
 ## Bootstrap F-test
-library(pbkrtest)
-anova(m2,m1)
-m1 <- lm(y ~ 1, dat)
-m2 <- lm(y ~ x1 + x2, dat)
-PBmodcomp(m2, m1, nsim = 1000, seed = 12345, )
-library(lmtest)
-lrtest(m1,m2)
+#library(pbkrtest)
+#anova(m2,m1)
+#m1 <- lm(y ~ 1, dat)
+#m2 <- lm(y ~ x1 + x2, dat)
+#PBmodcomp(m2, m1, nsim = 1000, seed = 12345, )
+#library(lmtest)
+#lrtest(m1,m2)
 
 # Bootstrapped se and CI coefficients
 library(car)
@@ -94,6 +96,21 @@ Confint(Boot(model, R = 100), type = "bca")
 anova(model)
 # anova geeft test voor hele categorische variabele, niet alleen voor dummies
 # handig!
+
+Ff <- c()
+lis <- list()
+for (i in 1:50000) {
+  bootstrap <- sample.int(100,100,replace = TRUE)
+  dat_bootstrap <- cbind.data.frame(y = dat$y, x1 = dat$x1[bootstrap], x2 = dat$x2[bootstrap])
+  mod <- lm(y ~ x1 + x2, dat_bootstrap)
+  Ff[i] <- summary(mod)$fstatistic[1]
+  lis[[i]] <- coefficients(mod)
+  print(i)
+}
+hist(Ff)
+sum(Ff >= summary(model)$fstatistic[1]) / length(Ff)
+
+
 
 
 ###### OUD
@@ -125,3 +142,12 @@ anova(model)
 # moeten zijn, dus heb je null model als het ware. Dan verzamel je F verdeling
 # en daarmee vergelijk je F die uit echte data komt.
 
+
+###########################################################################
+
+
+#library(lmPerm)
+
+#pmodel <- lmp(y ~ x1 + x2, dat, perm = "Exact")
+#pmodel <- lmp(y ~ x1 + x2, dat, perm = "")
+#summary(pmodel)

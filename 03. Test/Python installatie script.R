15c49e3857597c5a90dcdc85794708d16d6ff0cc
ElianGriffioen <e2.griffioen@vu.nl>
2021-02-12 09:39:01 +0100

    MEt package Python niet gebruikt

--- 03. Test/Python installatie script.R
+++ 03. Test/Python installatie script.R
@@ -17,6 +17,7 @@ py_install("pingouin")
 py_install("seaborn")
 py_install("scikit-posthocs", pip = TRUE)
 py_install("FisherExact", pip = TRUE)
+py_install("met", pip = TRUE)
 
 # https://github.com/rstudio/reticulate/issues/578
 # https://cran.r-project.org/web/packages/reticulate/vignettes/python_packages.html
@@ -26,13 +27,5 @@ py_install("FisherExact", pip = TRUE)
 # https://tedboy.github.io/statsmodels_doc/generated/statsmodels.stats.api.html (statsmodels.api)
 # https://pingouin-stats.org/index.html (pingouin)
 # https://docs.scipy.org/doc/scipy/reference/stats.html (scipy)
-
-# OUD
-#py_install("statsmodels.api")
-#conda_create("r-reticulate")
-#conda_install("statsmodels.api")
-#conda_remove("statsmodels")
-
-#use_python('C:\Users\egn225\Anaconda3\python.exe', required = T)
-
-#Sys.setenv(RETICULATE_PYTHON = 'C:/Users/egn225/Anaconda3')
+# https://pypi.org/project/met/
+# https://stackoverflow.com/questions/53159043/multinomial-test-in-python-with-met-module
